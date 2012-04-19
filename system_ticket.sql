3570cd4a38bd289685b364a6547c2f265a8069f0
zakala <zakala@interia.eu>
2012-04-19 17:29:31 +0200

    aktualizacja tabeli

--- system_ticket.sql
+++ system_ticket.sql
@@ -1,30 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 3.4.5deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Czas wygenerowania: 16 Kwi 2012, 11:42
--- Wersja serwera: 5.1.58
--- Wersja PHP: 5.3.6-13ubuntu3.6
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Baza danych: `system_ticket`
---
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla  `uzytkownik`
---
 
 CREATE TABLE IF NOT EXISTS `uzytkownik` (
   `ID` int(4) NOT NULL AUTO_INCREMENT,
@@ -36,14 +12,3 @@ CREATE TABLE IF NOT EXISTS `uzytkownik` (
   PRIMARY KEY (`ID`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=3 ;
 
---
--- Zrzut danych tabeli `uzytkownik`
---
-
-INSERT INTO `uzytkownik` (`ID`, `imie`, `nazwisko`, `login`, `haslo`, `mail`) VALUES
-(1, 'a', 'a', 'admin', 'admin', 'admin@a.pl'),
-(2, 'p', 'k', 'a', 'a', 'a@a.pl');
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
