79315d0d96389e6938c9485a7496473e4bba133d
zakala <zakala@interia.eu>
2012-04-23 10:14:50 +0200

    zmiencie haslo w uzytkownik

---
+++ kk/uzytkownik.php
@@ -0,0 +1,39 @@
+<?php
+$connect = mysql_connect('localhost', 'root', 'zakala2711');
+$wybierzBaze = mysql_select_db('system_ticket');
+
+if(!$connect)
+{
+	echo "Błąd podczas połączenia z bazą. Sprawdź parametry połączenia.";
+}
+
+if(isset($_POST['submit']))
+{
+	if($_POST['imie'] == '' && $_POST['nazwisko'] == '' && $_POST['login'] == '' && $_POST['haslo'] == '' && $_POST['mail'] == '')
+	{
+		echo "Wypełnij wszystkie pola !";
+	} else {
+		zapiszDane();
+	}
+}
+
+
+function zapiszDane()
+{
+        $imie = $_POST['imie'];
+	$nazwisko = $_POST['nazwisko'];
+	$login = $_POST['login'];
+	$haslo = $_POST['haslo'];
+	$mail = $_POST['mail'];
+
+$zapisz = mysql_query("INSERT INTO uzytkownik SET imie='".$imie."', nazwisko='".nazwisko."', login='".$login."', haslo='".$haslo."', mail='".$mail."'");
+
+if(!$zapisz)
+	{
+		echo "Dane nie zostały zapisane.";
+	} else {
+		echo "Dane zostały zapisane!";
+	}
+}
+?>
+
