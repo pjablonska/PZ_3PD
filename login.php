04078aa168f7ffa2d7594b01d83e0f5256d06aef
paulinaj <paulinaj@paulinaj-VirtualBox.(none)>
2012-05-26 14:55:51 +0200

    logowanie i wylogowanie

--- login.php
+++ login.php
@@ -1,77 +1,46 @@
-<?php
 
-session_start();
-
-if (isset($_POST['login']) and isset($_POST['haslo']) )
-
-{
-
-require('config.php');
-
-$login=mysql_real_escape_string(trim($_POST['login']));
-
-$haslo=mysql_real_escape_string(trim($_POST['haslo']));
-
-if ($login!="" and $haslo!="")
-
-{
-
-   $haslo = sha1($haslo);
-
-   $zapytanie="SELECT id FROM uzytkownik WHERE login='$login' and haslo ='$haslo'";
-
-   $temp=mysql_query($zapytanie) or die("Wystąpił błąd");
-
-    $ile=mysql_num_rows($temp);
-
-   $temp=mysql_fetch_array($temp);
-
-   $id=$temp['id'];
-
- 
-
-   if ($ile==1)
-
-   {
-
-     $_SESSION['user_id']=$id;
-
-     $_SESSION['login']=$konto;
-
-     echo('Zostales zalogowany. ');
-
-   }
-
-   else echo ('Podales zle dane. Kliknij wstecz aby sprobowac ponownie.');
-
-}
-
-}
-
-else{
-
-?>
-
-<html>
-<head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8">
-<form  action="ticket.html" method="post">
-<title>Logowanie</title>
 <body bgcolor="teal" text="white">
-<h1>Logowanie</h1><br><br>
-
-<table cellpadding="0" cellspacing="0" border="0">
-<tr><td>Login</td><td><input type="text" name="Login" value="" /></td></tr>
-<tr><td>Hasło</td><td><input type="password" name="haslo" value="" /></td></tr>
-<tr><td></td><td><input type="submit" name="submit" value="Zaloguj" /></td></tr>
-
-</form>
-
-</body>
-</head>
-</html>
-
 <?php
+session_start();
+mysql_connect('localhost','root','root');
+mysql_select_db('system_ticket');
 
-}?>
+if(isset($_SESSION['zalogowany'])) {
+
+}else{
+
+if(isset($_POST['wyslij'])) {
+
+
+   if(mysql_num_rows(mysql_query("SELECT login, haslo
+   FROM uzytkownik WHERE login = '".$_POST['login']."' 
+   && haslo = '".$_POST['haslo']."' ")) > 0) {
+
+
+      
+
+
+           $_SESSION['zalogowany'] = true;
+           $_SESSION['login'] = $_POST['login'];
+           $_SESSION['haslo'] = $_POST['haslo'];
+           echo "Jesteś zalogowany " .$_SESSION['login'];
+
+
+       } else { 
+
+   echo "Złe hasło, lub login proszę spróbować ponownie";
+}
+} else { 
+   echo "Nie ma takiego użytkownika";
+}
+} 
+
+if(isset($_POST['wyloguj'])) {
+session_destroy();
+echo "Zostałeś wylogowany " .$_SESSION['login'];
+}
+?>
+   
+
 
--- uzytkownik.php
+++ uzytkownik.php
@@ -1,5 +1,6 @@
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8">
+<body bgcolor="teal" text="white">
 </head> 
 <?php
 
