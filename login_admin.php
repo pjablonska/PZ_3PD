c37c4eaf0b93f69e0b12e65fcc170d678457aba0
paulinaj <paulinaj@paulinaj-VirtualBox.(none)>
2012-06-10 01:06:14 +0200

    dodanie logowania administratora

---
+++ login_admin.php
@@ -0,0 +1,45 @@
+
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<body bgcolor="teal" text="white">
+<?php
+session_start();
+mysql_connect('localhost','root','root');
+mysql_select_db('system_ticket');
+
+if(isset($_SESSION['zalogowany'])) {
+
+}else{
+
+if(isset($_POST['zaloguj'])) {
+
+
+   if(mysql_num_rows(mysql_query("SELECT login, haslo
+   FROM Admin WHERE login = '".$_POST['login']."' 
+   && haslo = '".$_POST['haslo']."' ")) > 0) {
+
+      
+
+           $_SESSION['zalogowany'] = true;
+           $_SESSION['login'] = $_POST['login'];
+           $_SESSION['haslo'] = $_POST['haslo'];
+          
+echo'<meta http-equiv="Refresh" content="0; url=Panel_admina.php" />';	
+
+       } else { 
+
+   echo "Złe hasło, lub login proszę spróbować ponownie";
+}
+} else { 
+   echo "Nie ma takiego użytkownika";
+}
+} 
+
+if(isset($_POST['wyloguj'])) {
+session_destroy();
+echo "Zostałeś wylogowany";
+echo'<meta http-equiv="Refresh" content="0; url=tytulowa.html" />';	
+}
+?>
+   
+
+
