6439ee04baae83cebb9e94fb98df6968ab8647e9
paulinaj <paulinaj@paulinaj-VirtualBox.(none)>
2012-06-10 01:05:34 +0200

    aktualizacja

--- login.php
+++ login.php
@@ -1,6 +1,8 @@
-
+<head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8">
+	
 <body bgcolor="teal" text="white">
+<center><b>
 <?php
 session_start();
 mysql_connect('localhost','root','root');
@@ -10,23 +12,19 @@ if(isset($_SESSION['zalogowany'])) {
 
 }else{
 
-if(isset($_POST['wyslij'])) {
-
+if(isset($_POST['zaloguj'])) {
 
    if(mysql_num_rows(mysql_query("SELECT login, haslo
    FROM uzytkownik WHERE login = '".$_POST['login']."' 
    && haslo = '".$_POST['haslo']."' ")) > 0) {
 
-
-      
-
-
            $_SESSION['zalogowany'] = true;
            $_SESSION['login'] = $_POST['login'];
            $_SESSION['haslo'] = $_POST['haslo'];
-           echo "Jesteś zalogowany " .$_SESSION['login'];
 
 
+
+echo '<meta http-equiv="Refresh" content="0; url=Panel_uzytkownika.php" />';
        } else { 
 
    echo "Złe hasło, lub login proszę spróbować ponownie";
@@ -35,12 +33,16 @@ if(isset($_POST['wyslij'])) {
    echo "Nie ma takiego użytkownika";
 }
 } 
-
 if(isset($_POST['wyloguj'])) {
 session_destroy();
-echo "Zostałeś wylogowany " .$_SESSION['login'];
+echo "Zostałeś wylogowany ";
+echo'<meta http-equiv="Refresh" content="1; url=tytulowa.html" />';
 }
+
 ?>
-   
+</b>
+</body>
+</head>
+ 
 
 
