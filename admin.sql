6b197c289f574a8dc5852d142fe5b3af37e33e9c
paulinaj <paulinaj@paulinaj-VirtualBox.(none)>
2012-06-10 00:57:54 +0200

    dodanie tabeli admin do bazy

---
+++ admin.sql
@@ -0,0 +1,14 @@
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+CREATE TABLE IF NOT EXISTS `Admin` (
+  `ID` int(4) NOT NULL AUTO_INCREMENT,
+  `imie` varchar(15) COLLATE utf8_polish_ci NOT NULL,
+  `nazwisko` varchar(25) COLLATE utf8_polish_ci NOT NULL,
+  `login` varchar(15) COLLATE utf8_polish_ci NOT NULL,
+  `haslo` varchar(15) COLLATE utf8_polish_ci NOT NULL,
+  `mail` varchar(25) COLLATE utf8_polish_ci NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
+
