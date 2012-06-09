153074eb1b1bb788d5f91977e159da042e4eec67
paulinaj <paulinaj@paulinaj-VirtualBox.(none)>
2012-06-10 01:06:45 +0200

    dodanie Panelu admina

---
+++ Panel_admina.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<title>Panel Administratora</title>
+<body bgcolor="teal" text="white">
+<center><h2>Panel Administratora</h2>
+<form method='POST' action='login.php'>
+<button type='submit' value='Wyloguj' name='wyloguj'><b>Wyloguj</b></button>
+</form>
+<form method='POST' action='Edycja_ticket.php'>
+<button type='submit' value='edytuj' name='edytuj'><b>Edytuj Zgłoszenia</b></button>
+</form>
+<form method='POST' action='edycja.php'>
+<button type='submit' value='edytuj' name='edytuj'><b>Edytuj Użytkowników</b></button>
+</form>
+<br>
+</body>
+</head>
+</html>
